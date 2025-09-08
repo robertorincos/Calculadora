d7d7b9be131f34339b2706d1a4c498ec243e5e80
robertorincos <beto.rinco.silveira@gmail.com>
2025-09-07 22:52:34 -0300

    Implementada funcionalidade de listar todas as operações a serem processadas.

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -16,17 +16,41 @@ namespace Calculadora
             filaOperacoes.Enqueue(new Operacoes { valorA = 18, valorB = 3, operador = '/' }); //Implemente o calculo de divisao
 
             Calculadora calculadora = new Calculadora();
+            List<Operacoes> todasOperacoes = new List<Operacoes>();
+            
+            foreach (var op in filaOperacoes)
+            {
+                todasOperacoes.Add(op);
+            }
 
             
             while (filaOperacoes.Count > 0)
             {
+                //Removendo e lendo o primeiro item da Queue.
                 Operacoes operacao = filaOperacoes.Dequeue();
                 calculadora.calcular(operacao);
+                
                 Console.WriteLine("{0} {1} {2} = {3}", operacao.valorA,operacao.operador,operacao.valorB, operacao.resultado);
+                
+                PrintProximasOperacoes(filaOperacoes);
+                Console.WriteLine();                
             }
 
           
            
+        }
+
+        static void PrintProximasOperacoes(Queue<Operacoes> filaOperacoes)
+        {
+            Console.WriteLine();
+            int contador = 1;
+            foreach (Operacoes op in filaOperacoes)
+            {
+                Console.WriteLine("{0}. {1} {2} {3}", 
+                    contador, op.valorA, op.operador, op.valorB);
+                contador++;
+            }
+
         }
     }
 }
