ee18657e7441d660637b7f07b4270a11fe438489
robertorincos <beto.rinco.silveira@gmail.com>
2025-09-07 23:08:35 -0300

    Criação stack de resultados.

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Calculadora
 {
@@ -13,10 +14,11 @@ namespace Calculadora
             filaOperacoes.Enqueue(new Operacoes { valorA = 14, valorB = 8, operador = '-' });
             filaOperacoes.Enqueue(new Operacoes { valorA = 5, valorB = 6, operador = '*' });
             filaOperacoes.Enqueue(new Operacoes { valorA = 2147483647, valorB = 2, operador = '+' });
-            filaOperacoes.Enqueue(new Operacoes { valorA = 18, valorB = 3, operador = '/' }); //Implemente o calculo de divisao
+            filaOperacoes.Enqueue(new Operacoes { valorA = 18, valorB = 3, operador = '/' });
 
             Calculadora calculadora = new Calculadora();
             List<Operacoes> todasOperacoes = new List<Operacoes>();
+            Stack<decimal> pilhaResultados = new Stack<decimal>();
             
             foreach (var op in filaOperacoes)
             {
@@ -26,9 +28,11 @@ namespace Calculadora
             
             while (filaOperacoes.Count > 0)
             {
-                //Removendo e lendo o primeiro item da Queue.
+                //Removendo e lendo o primeiro item da fila.
                 Operacoes operacao = filaOperacoes.Dequeue();
                 calculadora.calcular(operacao);
+                
+                pilhaResultados.Push(operacao.resultado);
                 
                 Console.WriteLine("{0} {1} {2} = {3}", operacao.valorA,operacao.operador,operacao.valorB, operacao.resultado);
                 
@@ -36,8 +40,12 @@ namespace Calculadora
                 Console.WriteLine();                
             }
 
-          
-           
+            //Print dos resultados.
+            foreach (decimal resultado in pilhaResultados.Reverse())
+            {
+                Console.WriteLine(resultado);
+            }
+            
         }
 
         static void PrintProximasOperacoes(Queue<Operacoes> filaOperacoes)
@@ -52,5 +60,6 @@ namespace Calculadora
             }
 
         }
+
     }
 }
